217c67cfb3a72d32cba0d3b7037d4f21fe7517bf
Lukasz Pieczonka <l.pieczonka@webcon.com>
2024-03-25 09:13:53 +0100

    19. Tuples

--- app.ts
+++ app.ts
@@ -1,21 +1,20 @@
-//TS representation of object type
-// const person: {
-//     name: string;
-//     age: number;
-// } = {
-const person ={
+const person:{
+    name:string;
+    age:number;
+    hobbies:string[];
+    role:[number,string];//tuple type - adds clarity of what type is expected.
+} ={
     name: 'zz',
     age: 30,
-    hobbies: ['sports', 'cooking']
+    hobbies: ['sports', 'cooking'],
+    role: [2,'author']
 }
 
-//let favoriteActivities:string[];
-//let favoriteActivities:any[];
-// favoriteActivities = ['swimming',1]
-
+person.role.push['admin']; // push works - its exception
+//person.role[1] = 10; // it is allowed without explicit tuple type definition in object because without it its only array of either string or number (string | number)[]
+//person.role = [0,'sadsa', 'a']// error
 for(const hobby of person.hobbies){
     console.log(hobby.toUpperCase())
-    //console.log(hobby.map()) error as type of nobby is string not array
 }
 
 
