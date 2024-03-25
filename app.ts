1fb3f151213fe662fa20be108372fe036717fb6b
Lukasz Pieczonka <l.pieczonka@webcon.com>
2024-03-25 08:27:08 +0100

    Working with number, string, boolean

--- app.ts
+++ app.ts
@@ -1,13 +1,16 @@
-function add(n1: number, n2: number){
-    //JS way of types validation
-    //if(typeof n1 ==='number' && typeof n2 ==='number'){
-    //    throw new Error('incorrect input!');
-    //}
-    return n1 + n2;
+function add(n1: number, n2: number, showResult: boolean, phrase: string){
+    const result = n1+n2;
+    if(showResult){
+        console.log(phrase+result);
+    }else{
+        return result;
+    }
 }
 
 const number1 = 5;
 const number2 = 2.8;
+const printResult = true;
+const resultPhrase =  'Result is: ';
 
-const result = add(number1,number2)
+const result = add(number1,number2, printResult, resultPhrase)
 console.log(result);
