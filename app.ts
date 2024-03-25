a852aeeb4de3c0ff3daa089af7238514eab8c7d9
Lukasz Pieczonka <l.pieczonka@webcon.com>
2024-03-25 09:03:13 +0100

    118. array types

--- app.ts
+++ app.ts
@@ -5,7 +5,18 @@
 // } = {
 const person ={
     name: 'zz',
-    age: 30
+    age: 30,
+    hobbies: ['sports', 'cooking']
 }
+
+//let favoriteActivities:string[];
+//let favoriteActivities:any[];
+// favoriteActivities = ['swimming',1]
+
+for(const hobby of person.hobbies){
+    console.log(hobby.toUpperCase())
+    //console.log(hobby.map()) error as type of nobby is string not array
+}
+
 
 console.log(person.name)
