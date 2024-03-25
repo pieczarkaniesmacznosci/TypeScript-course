37345b1894349eda7d4b4b605948b3edb32feed3
Lukasz Pieczonka <l.pieczonka@webcon.com>
2024-03-25 09:50:34 +0100

    20. Enum type

--- app.ts
+++ app.ts
@@ -1,21 +1,23 @@
-const person:{
-    name:string;
-    age:number;
-    hobbies:string[];
-    role:[number,string];//tuple type - adds clarity of what type is expected.
-} ={
+enum Role{
+    ADMIN, READ_ONLY, AUTHOR
+}
+
+const person ={
     name: 'zz',
     age: 30,
     hobbies: ['sports', 'cooking'],
-    role: [2,'author']
+    role:Role.ADMIN
 }
 
-person.role.push['admin']; // push works - its exception
+// person.role.push['admin']; // push works - its exception
 //person.role[1] = 10; // it is allowed without explicit tuple type definition in object because without it its only array of either string or number (string | number)[]
-//person.role = [0,'sadsa', 'a']// error
+//person.role = [0,'worker', 'a']// error
 for(const hobby of person.hobbies){
     console.log(hobby.toUpperCase())
 }
 
+if(person.role === Role.ADMIN){
+    console.log('its admin')
+}
 
 console.log(person.name)
